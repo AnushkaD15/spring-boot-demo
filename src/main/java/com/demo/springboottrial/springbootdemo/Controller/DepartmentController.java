a7fc9812980ff79a813c5138da7e38fb6e4c1b85
AnushkaD15 <storoid1508@gmail.com>
2023-07-30 23:58:13 +0530

    third

--- src/main/java/com/demo/springboottrial/springbootdemo/Controller/DepartmentController.java
+++ src/main/java/com/demo/springboottrial/springbootdemo/Controller/DepartmentController.java
@@ -3,9 +3,11 @@ package com.demo.springboottrial.springbootdemo.Controller;
 
 import com.demo.springboottrial.springbootdemo.entity.Department;
 import com.demo.springboottrial.springbootdemo.service.DepartmentService;
-import com.demo.springboottrial.springbootdemo.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.management.DescriptorAccess;
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -16,5 +18,26 @@ public class DepartmentController {
     public Department saveDepartment(@RequestBody Department department){
 
         return departmentService.saveDepartment(department);
+    }
+    @GetMapping(value = "/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartment();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted successfully!!";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId,
+                                       @RequestBody Department department) {
+        return departmentService.updateDepartment(departmentId,department);
     }
 }
--- src/main/java/com/demo/springboottrial/springbootdemo/service/DepartmentService.java
+++ src/main/java/com/demo/springboottrial/springbootdemo/service/DepartmentService.java
@@ -2,7 +2,16 @@ package com.demo.springboottrial.springbootdemo.service;
 
 import com.demo.springboottrial.springbootdemo.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
 
+    public List<Department> fetchDepartment();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
 }
