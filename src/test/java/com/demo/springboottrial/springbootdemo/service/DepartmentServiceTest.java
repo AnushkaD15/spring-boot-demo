8211a23eb8f4166a1dabab8bfd663dc21f85276a
AnushkaD15 <storoid1508@gmail.com>
2023-07-31 18:11:50 +0530

    5

---
+++ src/test/java/com/demo/springboottrial/springbootdemo/service/DepartmentServiceTest.java
@@ -0,0 +1,15 @@
+package com.demo.springboottrial.springbootdemo.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}
